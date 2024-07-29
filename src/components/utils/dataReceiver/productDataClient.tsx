bbc171baba8c69137661076ac8ab3c5570fc86e7
Artur Troyan <arturtroyandev@gmail.com>
2024-07-29 10:26:17 +0300

    Addition and modification of old logic

--- src/components/utils/dataReceiver/productDataClient.tsx
+++ src/components/utils/dataReceiver/productDataClient.tsx
@@ -1,26 +1,7 @@
 "use client"
 import React, { useEffect } from "react"
-import { useContextValue } from "@/context/ContextValue"
 import { useSelector } from 'react-redux'
 import type { RootState } from '../../utils/redux/store'
-
-
-
-// export const productDataClient = () => {
-//     const [items, setItems] = React.useState<any>([])
-//     const { categoryId, setCategoryId } = useContextValue();
-
-//     React.useEffect(() => {
-//         fetch("http://localhost:1337/api/chebureks?&filters[category]=" + 2)
-//             .then((res) => res.json())
-//             .then((arr) => {
-//                 // const obj = Object.values(arr)
-//                 setItems(arr);
-//             })
-//     }, [categoryId])
-
-//     return items
-// }1
 
 type ImageAttributes = {
     url: string,
@@ -61,7 +42,6 @@ interface ExtractedData {
 
 export const productDataClient = () => {
     const [items, setItems] = React.useState<ExtractedData[]>([]);
-    // const { categoryId, setCategoryId } = useContextValue();
     const categoryId = useSelector((state: RootState) => state.filter.categoryId)
 
 
@@ -91,86 +71,5 @@ export const productDataClient = () => {
 
     return items
 }
-
-
-
-
-
-
-export const productDataImageClient = () => {
-    const [items, setItems] = React.useState<any>([])
-    // const { categoryId, setCategoryId } = useContextValue();
-
-    React.useEffect(() => {
-        fetch("http://localhost:1337/api/chebureks?populate=*")
-            .then((res) => res.json())
-            .then((arr) => {
-                const obj = Object.values(arr)
-                setItems(obj[0]);
-            })
-    }, [])
-
-    return items
-}
-
-// let data = productDataClient()
-
-// data.map((obj) => {
-//     console.log(obj)
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-// (async (categoryId?: number): Promise<string[]> => {
-//     // const filter = "1"
-//     // console.log(categoryId)
-
-//     const response = await fetch("http://localhost:1337/api/chebureks?&filters[category]=" + categoryId)
-//     // http://localhost:1337/api/chebureks?&filters[category]=ponchik
-//     // http://localhost:1337/api/chebureks?populate=*
-//     const chebureks = await response.json()
-//     const res = chebureks.data
-//     return res
-// })// fix the  function type
-
-// export function getChebureks({
-//     chebureks,
-// }: InferGetStaticPropsType<typeof getStaticProps>) {
-//     return chebureks
-// }
-
-
-// export const getImageUrl = (async (): Promise<[]> => {
-//     const response = await fetch("http://localhost:1337/api/chebureks?populate=*")
-//     const chebureks = await response.json()
-//     const data = chebureks.data
-//     const arr: any = [];
-
-//     data.map((obj: any) => {
-//         arr.push(obj.attributes.image.data[0].attributes.url)
-//     })
-
-//     // console.log(arr)
-//     return arr
-
-// })
-
-
-
-
-
-
-
-
 
 
